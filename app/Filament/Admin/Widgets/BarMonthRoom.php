e841de51278e1bf8f1e0b88ab6573e4242f10d73
Adilonapsh <lubos418@gmail.com>
2025-01-13 13:06:51 +0700

    Push Dashboard

--- app/Filament/Admin/Widgets/BarMonthRoom.php
+++ app/Filament/Admin/Widgets/BarMonthRoom.php
@@ -2,7 +2,9 @@
 
 namespace App\Filament\Admin\Widgets;
 
+use App\Models\RoomReportHistory;
 use Filament\Widgets\ChartWidget;
+use Illuminate\Support\Facades\DB;
 
 class BarMonthRoom extends ChartWidget
 {
@@ -12,16 +14,47 @@ class BarMonthRoom extends ChartWidget
 
     protected function getData(): array
     {
+        // $kamar_terisi = RoomReportHistory::groupBy('created_at','id')
+        //     ->get()
+        //     // ->pluck('total_terisi', 'month')
+        //     ->toArray();
+        $kamarTerisi = RoomReportHistory::selectRaw('
+                EXTRACT(MONTH FROM created_at) as month,
+                SUM((data_history->>\'guests_count\')::INTEGER) as total_guests_count,
+                SUM((data_history->>\'tersisa\')::INTEGER) as total_tersisa
+            ')
+            ->groupBy(DB::raw('EXTRACT(MONTH FROM created_at)'))
+            ->get();
+
+        $kamar_terisi = $kamarTerisi->pluck("total_guests_count", 'month')->toArray();
+        $kamar_tersisa = $kamarTerisi->pluck("total_tersisa", 'month')->toArray();
+
+        $bulan_terisi = [];
+        $bulan_tersisa = [];
+        for ($i = 1; $i <= 12; $i++) {
+            $bulan_terisi[$i] = 0;
+            $bulan_tersisa[$i] = 0;
+        }
+        foreach ($kamar_terisi as $key => $value){
+            $bulan_terisi[$key] = $value;
+        }
+        foreach ($kamar_tersisa as $key => $value){
+            $bulan_tersisa[$key] = $value;
+        }
+
+        $kamar_terisi = array_values($bulan_terisi);
+        $kamar_tersisa = array_values($bulan_tersisa);
+
         return [
             'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
             'datasets' => [
                 [
                     'label' => 'Terisi',
-                    'data' => [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120],
+                    'data' =>  $kamar_terisi,
                 ],
                 [
                     'label' => 'Kosong',
-                    'data' => [120, 110, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10],
+                    'data' => $kamar_tersisa,
                 ],
             ],
         ];
--- app/Filament/Admin/Widgets/StatusRoomData.php
+++ app/Filament/Admin/Widgets/StatusRoomData.php
@@ -2,7 +2,9 @@
 
 namespace App\Filament\Admin\Widgets;
 
+use App\Models\Room;
 use Filament\Widgets\ChartWidget;
+use Illuminate\Support\Facades\DB;
 
 class StatusRoomData extends ChartWidget
 {
@@ -12,12 +14,47 @@ class StatusRoomData extends ChartWidget
 
     protected function getData(): array
     {
+
+        $query = Room::query()
+            ->select(
+                'rooms.*',
+                DB::raw('MIN(guests.tanggal_checkin) as tanggal_checkin_awal'),
+                DB::raw('MAX(guests.tanggal_checkout) as tanggal_checkout_terakhir'),
+                DB::raw('(rooms.kapasitas - COUNT(guests.id)) as tersisa'),
+            )
+            ->leftJoin('guests', function ($join) {
+                $join->on('rooms.id', '=', 'guests.id_kamar')
+                    ->whereNotNull('guests.tanggal_checkin')
+                    ->whereNull('guests.tanggal_checkout');
+            })
+            ->groupBy('rooms.id')
+            ->with(['guests' => function ($query) {
+                $query->select(
+                    'id',
+                    'nama',
+                    'id_kamar',
+                    'id_kelas',
+                    'batch',
+                    'tanggal_checkin',
+                    'tanggal_checkout'
+                )
+                    ->whereNotNull('guests.tanggal_checkin')
+                    ->whereNull('guests.tanggal_checkout')
+                    ->orderBy('tanggal_checkin');
+            }])
+            ->withCount(['guests' => function ($query) {
+                $query->whereNotNull('tanggal_checkin')
+                    ->whereNull('tanggal_checkout');
+            }]);
+
+        $kamarTerisi = $query->get()->sum('guests_count') ?? 0;
+        $kamarKosong = Room::sum('kapasitas') - $kamarTerisi ?? 0;
         return [
-            'labels' => ['Available', 'Occupied', 'Reserved'],
+            'labels' => ['Kamar Terisi', 'Kamar Kosong'],
             'datasets' => [
                 [
-                    'data' => [10, 5, 3],
-                    'backgroundColor' => ['#4CAF50', '#FF9800', '#F44336'],
+                    'data' => [$kamarTerisi, $kamarKosong],
+                    'backgroundColor' => ['#4CAF50', '#FF9800'],
                 ],
             ],
         ];
